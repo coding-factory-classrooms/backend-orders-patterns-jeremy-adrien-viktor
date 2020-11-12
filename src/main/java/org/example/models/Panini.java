c5b1e1c475230cabd216e46a1348a69c9c26f070
Adri <adrigrand@yahoo.fr>
2020-11-12 11:53:56 +0100

    1#
    Affichage des paninis

--- src/main/java/org/example/models/Panini.java
+++ src/main/java/org/example/models/Panini.java
@@ -1,4 +1,13 @@
 package org.example.models;
 
+import java.util.List;
+
 public class Panini extends Dish{
+
+    public Panini(String name, List<Ingredient> ingredientList, float price, String desc) {
+        this.setName(name);
+        this.setIngredientList(ingredientList);
+        this.setPrice(price);
+        this.setDescription(desc);
+    }
 }

b30b3ed24ca95d29231106f581fc806448320154
Adri <adrigrand@yahoo.fr>
2020-11-12 21:01:55 +0100

    5#
    Début de la tâche avec lien vers la page de détail de la commande

--- src/main/java/org/example/OrdersSystem.java
+++ src/main/java/org/example/OrdersSystem.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.models.Dish;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class OrdersSystem {
@@ -14,8 +15,14 @@ public class OrdersSystem {
     }
 
     public Order createOrderAndReturn(List<Dish> dishes) {
+        float sum = 0;
+        for(int i = 0; i < dishes.size(); i++)
+            sum += dishes.get(i).getPrice();
+
         Order order = new Order();
         order.setDishes(dishes);
+        order.setDateOrder(new Date());
+        order.setTotalPrice(sum);
         order.setState(Order.State.NEW);
         orderList.add(order);
         return orderList.get(orderList.size() -1);
@@ -23,5 +30,9 @@ public class OrdersSystem {
 
     public OrdersSystem() {
         this.orderList = new ArrayList<>();
+    }
+
+    public Order getOrder(int index) {
+        return orderList.get(index);
     }
 }
